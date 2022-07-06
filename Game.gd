a62587360388c71ab649961a21ca4463a4d755c6
Bogdan <31617323+burinss@users.noreply.github.com>
2022-07-06 22:51:39 +0300

    Сamera dragging

--- Game.gd
+++ Game.gd
@@ -4,34 +4,40 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var drag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x=100
 	position.y=100
 	var dict = {}
-	#var body = StaticBody2D.new()
 	var file = File.new()
 	file.open("res://data1.json", file.READ)
 	var text = file.get_as_text()
-	#print(dict.parse_json(text))
 	var result_json = JSON.parse(text).result
 	file.close()
-# print something from the dictionnary for testing.
+	
 	var entities = result_json["entities"]
+	
 	for entity in entities:
 		var mySprite = Sprite.new()
-		#mySprite.texture = load("res://cropped//Albania_province_1.png")
 		mySprite.texture = load("res://cropped//" + entity['Name'])
 		mySprite.position = Vector2(entity['x'],entity['y'])
-		#mySprite.rotate(angle)
 		mySprite.scale = Vector2(1.0,1.0)
 		mySprite.centered=false
 		add_child(mySprite)
 	#pass # Replace with function body.
 
-
+func _unhandled_input(event):
+	if event.is_action_pressed("click",false):
+		drag = true
+	elif event.is_action_released("click"):
+		drag=false
+	elif event.is_class("InputEventMouseMotion") and drag:
+		$Camera2D.position-=event.relative
+		
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
+#	$Camera2D.position = init_camera_pos - (get_global_mouse_position()-drag_position)
 #	pass
