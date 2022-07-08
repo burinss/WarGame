24758c510839910f02c754ce60159755910b91e0
satiriorn <45328793+satiriorn@users.noreply.github.com>
2022-07-08 11:51:00 +0300

    fixed map

--- Game.gd
+++ Game.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var drag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,24 +20,40 @@ func _ready():
 	var entities = result_json["entities"]
 	
 	for entity in entities:
+		var myArea = Area2D.new()
+		myArea.position = Vector2(entity['x'],entity['y'])
 		var mySprite = Sprite.new()
-		var script = load('res://Sprite.gd')
-		var texture = load("res://cropped//" + entity['Name'])
-		mySprite.texture = texture
-		mySprite.position = Vector2(entity['x'],entity['y'])
+		var image = Image.new()
+		image.load("res://cropped//" + entity['Name'])
+		mySprite.texture = load("res://cropped//" + entity['Name'])
+		#mySprite.position = Vector2(entity['x'],entity['y'])
 		mySprite.scale = Vector2(1.0,1.0)
 		mySprite.centered=false
-		mySprite.set_script(script)
-		add_child(mySprite)
-
+		var script = load('res://Sprite.gd')
+		set_collision(image, myArea)
+		myArea.set_script(script)
+		myArea.add_child(mySprite)
+		add_child(myArea)
+		
+	for x in self.get_children():
+		print(str(x))
+		#add_child(myArea)
+	#pass # Replace with function body.
+func set_collision(x, myArea):
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(x)
+	var collider = CollisionPolygon2D.new()
+	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0, 0), bitmap.get_size()))
+	for polygon in polygons:
+		collider.polygon = polygon
+	myArea.add_child(collider)
+		
 func _unhandled_input(event):
-	if event is InputEventSingleScreenTouch:
-		print("click")
 	if event is InputEventSingleScreenDrag:
 		$Camera2D.position-=event.relative
+		
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	$Camera2D.position = init_camera_pos - (get_global_mouse_position()-drag_position)
 #	pass
-
--- Sprite.gd
+++ Sprite.gd
@@ -1,32 +1,48 @@
-extends Sprite
+extends Area2D
 
+onready var the_sprite = get_node("path/to/sprite")
 
+func _ready() -> void:
+	input_pickable = true
+	self.connect("input_event", self, "_on_Area2D_input_event")
+
+func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		print("Clicked")
+		print(str(get_children()))
+		if event.pressed:
+			modulate = Color.blueviolet
+		else:
+			modulate = Color.white
+
+		
 func _input(event):
 	var drag = false
 	if event is InputEventSingleScreenDrag:
 		drag = true
+	"""
 	#OG code. It works but there are more then 2 ways to skin a cat.
 	if event is InputEventSingleScreenTouch:
-		if get_rect().has_point(to_local(event.position)):
+		if get_rect().has_point(get_local_mouse_position()):
 			print("A click!")
-		
-	"""
+
+	
 	#good if you dont need to do something when the button is released
-	if InputEventSingleScreenTouch :
+	if InputEventSingleScreenTouch:
 		if get_rect().has_point(get_local_mouse_position()):
 			get_tree().set_input_as_handled()
 			modulate = Color.blueviolet
-	"""
+	
 	#if you need to do something when the buttons released
 	if event is InputEventSingleScreenTouch:
-		if get_rect().has_point(get_local_mouse_position()):
+		if get_viewport_rect().has_point(get_local_mouse_position()):
 			get_tree().set_input_as_handled()
 			if event.pressed:
 					modulate = Color.blueviolet
 			else:
 				modulate = Color.white
+"""
 
-	
 	#if you need to only select opague parts of the sprite replace get_rect()
 	#with is_pixel_opaque(get_local_mouse_position())
 
