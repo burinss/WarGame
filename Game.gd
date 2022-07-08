eb00789ec835fa83df324b3435d6281e5a533066
satiriorn <45328793+satiriorn@users.noreply.github.com>
2022-07-08 13:54:31 +0300

    add water

--- Game.gd
+++ Game.gd
@@ -5,7 +5,8 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var drag = false
-
+var name_state
+var arr_color_state = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x=100
@@ -18,25 +19,27 @@ func _ready():
 	file.close()
 	
 	var entities = result_json["entities"]
-	
+		
 	for entity in entities:
 		var myArea = Area2D.new()
 		myArea.position = Vector2(entity['x'],entity['y'])
 		var mySprite = Sprite.new()
 		var image = Image.new()
 		image.load("res://cropped//" + entity['Name'])
+		name_state = entity['Name'].split("_")[0]
+
+		mySprite.centered=false
 		mySprite.texture = load("res://cropped//" + entity['Name'])
 		#mySprite.position = Vector2(entity['x'],entity['y'])
 		mySprite.scale = Vector2(1.0,1.0)
-		mySprite.centered=false
+		
+		set_color_province(mySprite)
 		var script = load('res://Sprite.gd')
 		set_collision(image, myArea)
 		myArea.set_script(script)
 		myArea.add_child(mySprite)
 		add_child(myArea)
 		
-	for x in self.get_children():
-		print(str(x))
 		#add_child(myArea)
 	#pass # Replace with function body.
 func set_collision(x, myArea):
@@ -47,7 +50,19 @@ func set_collision(x, myArea):
 	for polygon in polygons:
 		collider.polygon = polygon
 	myArea.add_child(collider)
-		
+	
+func set_color_province(sprite):
+	var water = false
+	if "Water" in name_state:
+		water = true
+	if name_state in arr_color_state.keys() and water==false:
+		sprite.modulate = arr_color_state[name_state]
+	elif water==false:
+		var c = Color(rand_range(0.0, 1.0), rand_range(0.0, 1.0), rand_range(0.0, 1.0))
+		sprite.modulate = c
+		arr_color_state[name_state] = c
+
+	
 func _unhandled_input(event):
 	if event is InputEventSingleScreenDrag:
 		$Camera2D.position-=event.relative
